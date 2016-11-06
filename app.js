7740a5782a9486112d28b0d3ce3acf4fb429da7f
李学江 <lixuejiang@ggbond-mac.local>
2016-11-06 11:23:59 +0800

    add file

---
+++ app.js
@@ -0,0 +1,19 @@
+/**
+ * Created by lixuejiang on 15/9/4.
+ */
+var agent = require("superagent"),
+    cheerio = require('cheerio'),
+    url = process.argv[2];
+
+agent.get(url)
+     .end(function(err,res){
+        if(err) throw err;
+        //console.log(res);
+        var $ = cheerio.load(res.text),
+            aList = $("ol li a"),info_list = [];
+
+        aList.each(function(index,data){
+            var $a = $(data);
+            console.log($a.attr('href'));
+        });
+    });
